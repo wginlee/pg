49bd7054459f60929f607b431a01ed069dc2ccce
Graham Lee <wgin.lee@gmail.com>
2016-11-22 20:13:52 +0000

    refactor

--- lookup_people.js
+++ lookup_people.js
@@ -12,20 +12,26 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
-client.connect((err) => {
+function queryDb(sql, params, callback){
+  client.connect((err) => {
+    if (err) return console.error("error connecting to database", err);
+    client.query(sql, params, (err, result) => {
+      if (err) return callback(err);
+      callback(null, result);
+      client.end((err) => {
+        if (err) throw err;
+      });
+    });
+  });
+}
+
+queryDb("SELECT id, first_name, last_name, birthdate FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [args[2]], (err, result) => {
   if (err) {
-    return console.error("Connection Error", err);
+    return console.error("error running query", err);
   }
   console.log("Searching...");
-  client.query("SELECT id, first_name, last_name, birthdate FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [args[2]], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    console.log("Found", result.rows.length, "person(s) by the name", args[2]);
-    for (var row in result.rows){
-
-      console.log(result.rows[row].id, ":", result.rows[row].first_name, result.rows[row].last_name, ", born", result.rows[row].birthdate.toDateString());
-    }
-    client.end();
-  });
+  console.log("Found", result.rows.length, "person(s) by the name", args[2]);
+  for (var row in result.rows){
+    console.log(result.rows[row].id, ":", result.rows[row].first_name, result.rows[row].last_name, ", born", result.rows[row].birthdate.toDateString());
+  }
 });
